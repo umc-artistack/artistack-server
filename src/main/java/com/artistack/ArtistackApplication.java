5028d506d6b2fc589858f8dee80082905ebe2708
defwdahyun0 <wdh112139@gmail.com>
2022-07-15 17:19:36 +0900

    feat: set springboot project

---
+++ src/main/java/com/artistack/ArtistackApplication.java
@@ -0,0 +1,13 @@
+package com.artistack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtistackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtistackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/artistack/ArtistackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.artistack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtistackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
