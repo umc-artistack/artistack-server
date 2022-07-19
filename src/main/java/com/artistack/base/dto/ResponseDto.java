e3821844c141394cb44e094d878c04776fa8f102
Mason Kim <mskim9967@gmail.com>
2022-07-19 12:54:50 +0900

    [#6] feat: Response 형식 지정

---
+++ src/main/java/com/artistack/base/dto/ResponseDto.java
@@ -0,0 +1,26 @@
+package com.artistack.base.dto;
+
+import com.artistack.base.constant.Code;
+import lombok.*;
+
+@Getter
+@ToString
+@RequiredArgsConstructor
+public class ResponseDto {
+
+    private final Boolean success;
+    private final Integer code;
+    private final String message;
+
+    public static ResponseDto of(Boolean success, Code code) {
+        return new ResponseDto(success, code.getCode(), code.getMessage());
+    }
+
+    public static ResponseDto of(Boolean success, Code errorCode, Exception e) {
+        return new ResponseDto(success, errorCode.getCode(), errorCode.getMessage(e));
+    }
+
+    public static ResponseDto of(Boolean success, Code errorCode, String message) {
+        return new ResponseDto(success, errorCode.getCode(), errorCode.getMessage(message));
+    }
+}
--- src/main/java/com/artistack/user/controller/UserController.java
+++ src/main/java/com/artistack/user/controller/UserController.java
@@ -1,4 +1,83 @@
 package com.artistack.user.controller;
 
+import com.artistack.base.dto.DataResponseDto;
+import com.artistack.base.GeneralException;
+import com.artistack.base.constant.Code;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/user")
+@Slf4j
 public class UserController {
+
+    // http://localhost:8080/user
+    @GetMapping(path = "")
+    public DataResponseDto<Object> get() {
+        return DataResponseDto.of(List.of(1, 2, 3));
+        /*
+          {
+            "success": true,
+            "code": 0,
+            "message": "Ok",
+            "data": [
+              1,
+              2,
+              3
+            ]
+          }
+         */
+    }
+
+
+    // http://localhost:8080/user/error/custom
+    @GetMapping(path = "/error/custom")
+    public DataResponseDto<Object> errorWithCustomException() {
+        Boolean isValid = false;
+
+        if (!isValid) {
+            // exception occurs
+            throw new GeneralException(Code.VALIDATION_ERROR, "Reason why it isn't valid");
+            /*
+              {
+                "success": false,
+                "code": 10001,
+                "message": "Validation error - Reason why it isn't valid"
+              }
+            */
+        }
+
+        return DataResponseDto.empty();
+    }
+
+
+    // http://localhost:8080/user/error/exception
+    @GetMapping(path = "/error/exception")
+    public DataResponseDto<Object> errorWithException() {
+        try {
+            List<Integer> list = List.of(1, 2, 3, 4, 5);
+
+            log.debug(list.get(99999).toString()); // outofbound exception occurs
+
+        } catch (Exception e) {
+            log.trace("Exception", e);
+            throw e;
+                /*
+                  {
+                    "success": false,
+                    "code": 20000,
+                    "message": "Internal error - Index 9 out of bounds for length 5"
+                  }
+                */
+        }
+
+        return DataResponseDto.empty();
+    }
 }
