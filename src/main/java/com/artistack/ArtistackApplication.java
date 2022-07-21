9542ec1502db0fceaf839d155a5278e5f1d6c898
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-07-21 21:48:18 +0900

    [#9] feat: S3 파일 업로드 관련 파일 추가

--- src/main/java/com/artistack/ArtistackApplication.java
+++ src/main/java/com/artistack/ArtistackApplication.java
@@ -2,13 +2,19 @@ package com.artistack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 public class ArtistackApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ArtistackApplication.class, args);
-	}
+	public static final String APPLICATION_LOCATIONS = "spring.config.location="
+		+"classpath:application.properties,"
+		+"classpath:aws.yml";
 
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ArtistackApplication.class)
+			.properties(APPLICATION_LOCATIONS)
+			.run(args);
+	}
 }
