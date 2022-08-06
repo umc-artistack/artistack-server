6be68e9fb4516eded67dca40f97322fe6cd7d355
leeeeeyeon <leeeeeyeonn@gmail.com>
2022-08-06 13:00:47 +0900

    [#4] style: 회원가입, 로그인 Swagger 어노테이션 추가

--- src/main/java/com/artistack/oauth/controller/OAuthController.java
+++ src/main/java/com/artistack/oauth/controller/OAuthController.java
@@ -6,6 +6,8 @@ import com.artistack.jwt.dto.JwtDto;
 import com.artistack.oauth.constant.ProviderType;
 import com.artistack.oauth.service.OAuthService;
 import com.artistack.user.dto.UserDto;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -25,6 +27,7 @@ public class OAuthController {
 
     private final OAuthService oAuthService;
 
+    @ApiOperation(value = "로그인")
     @GetMapping("/signIn")
     public DataResponseDto<Object> loginWithProviderToken(
         @RequestParam("providerType") ProviderType providerType,
@@ -36,6 +39,8 @@ public class OAuthController {
         return DataResponseDto.of(Code.NOT_REGISTERED, signInResult);
     }
 
+    @ApiOperation(value = "회원 가입")
+    @ApiImplicitParam(name = "providerType", value = "회원가입 타입", required = true, dataType = "string", paramType = "query")
     @GetMapping("/signUp")
     public DataResponseDto<Object> signUp(
         @RequestHeader("Authorization") String providerAccessToken,
